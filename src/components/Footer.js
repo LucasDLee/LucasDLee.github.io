b617966b174298d2e2d31d8ab1f84eeeed23fc71
Lucas Lee <LucasDLee23@gmail.com>
2023-01-05 14:49:03 -0800

    updated linkedin link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
         <footer>
             <ul>
                 <li><a href="https://github.com/LucasDLee"><img src="/images/company-icons/github.webp" alt="github account" height="50" width="50" /></a></li>
-                <li><a href="https://www.linkedin.com/in/lucas-l-7b0bb3201/"><img src="/images/company-icons/linkedin.webp" alt="linkedin account" height="50" width="50" /></a></li>
+                <li><a href="https://www.linkedin.com/in/lucas-l-7b0bb3201/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BgVA3WF3zSNeKpnpbNOuf5A%3D%3D"><img src="/images/company-icons/linkedin.webp" alt="linkedin account" height="50" width="50" /></a></li>
             </ul>
         </footer>
     )
