1e3fbd8cb4881feb9447260986057b6405c78c18
Lucas Lee <99562845+LucasDLee@users.noreply.github.com>
2024-01-04 18:19:41 -0800

    Swapped browserrouter to hashrouter (#17)

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
         <header id="home-header">
             <h1>Hello, I'm Lucas, <br></br>a software developer.</h1>
             <p>Here you'll find information about myself, the projects I have worked on and the places I have worked at.</p>
-            <div class="arrow down-arrow down-animation"></div>
+            <div className="arrow down-arrow down-animation"></div>
         </header>
     );
 }
